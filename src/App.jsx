85d6175a3358ff10a993f716903690e8ef821fd6
KL2400030537 <2400030537@kluniversity.in>
2025-07-23 08:03:59 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1><b>This is second</b></h1>
+      </div>
+    );
+  }
+}
+
+export default App;
